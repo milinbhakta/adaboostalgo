2e8682e9ebc0f0cb3c457e5402b884ca6f160bd1
milinbhakta <desire2dosomething@gmail.com>
2016-01-30 14:55:40 +0530

    ver: 1.0

---
+++ your_algorithm.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import sys
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn import svm
+from time import time
+sys.path.append("../tools/")
+
+
+
+t2 = time()
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+#plt.show()
+################################################################################
+
+
+### your code here!  name your classifier object clf if you want the
+### visualization code (prettyPicture) to show you the decision boundary
+
+#clf = KNeighborsClassifier(weights='uniform')
+#clf.fit(features_train, labels_train)
+#clf = AdaBoostClassifier()
+#clf.fit(features_train, labels_train)
+print "preprocess time:", round(time()-t2, 3), "s"
+clf = AdaBoostClassifier()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+
+pred = clf.predict(features_test)
+accuracy = clf.score(features_test, labels_test)
+print "prediction time:", round(time()-t1, 3), "s"
+print 'Accuracy:', accuracy
+
+
+
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
